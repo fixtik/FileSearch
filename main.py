fa61907c798d97d8d202cf1a8574c06ed00d1c06
Alex <alx@mail.ru>
2022-07-08 16:41:39 +0300

    add preview window

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import sys
 import datetime
 import mmap
 import time
+import shutil
 
 from PySide6 import QtCore, QtWidgets, QtGui
 
 from ui.search_form import Ui_MainWindow
+from ui.previewWindow import PrevieWindow
 
 
 class Form_backend(QtWidgets.QMainWindow):
@@ -19,15 +21,25 @@ class Form_backend(QtWidgets.QMainWindow):
         self.initThreads()
 
     def initUi(self):
+        """
+        Инициализация начальных значений пользовательского интефейса
+        """
+        self.setWindowTitle('Поиск файлов')
         self.ui.stopSearchpushButton.setEnabled(False)
+
         self.ui.tableView.setModel(self.createQStandardItemModel()) # добавление заголовков таблицы
+        self.ui.tableView.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)  # изменение ширины первого столбца
         self.ui.tableView.setVisible(False)
+        self.ui.tableView.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers) # запрет на редактирование ячейки
+
+        self.errorMsg = QtWidgets.QErrorMessage(self)
 
         # установка параметров для treeView
         model = QtWidgets.QFileSystemModel()
         model.setRootPath(QtCore.QDir.currentPath())
         self.ui.treeView.setModel(model)
-        self.ui.treeView.clicked.connect(self.changeDir)
+        self.ui.treeView.setMinimumSize(300, 200)  # минимальный размер
+        self.ui.treeView.setColumnWidth(0, 200)    # ширина первой колонки
 
         # заполнение entringStringLabel и lineEdit
         self.changeText()
@@ -36,9 +48,22 @@ class Form_backend(QtWidgets.QMainWindow):
         self.ui.chooseSettingComboBox.currentTextChanged.connect(self.changeText)
         self.ui.startSearchpushButton.clicked.connect(self.startSearchButtonClick)
         self.ui.stopSearchpushButton.clicked.connect(self.stopSearchButtonClick)
+        self.ui.tableView.doubleClicked.connect(self.table_view_dbl_click)
+        self.ui.treeView.clicked.connect(self.changeDir)
+
+    def initThreads(self):
+        """
+        инициализация потока
+        """
+        self.findfileThread = TFindFileThread()
+        self.findfileThread.infoSignal.connect(self.addItemToResultTable)
+        self.findfileThread.statusSignal.connect(self.showProcessInStatusBar)
+        self.findfileThread.finished.connect(self.stopSearchButtonClick)
 
     def changeText(self):
-        """устанавливает текст entringStringLabel и lineEdit в зависимости от chooseSettingComboBox"""
+        """
+        устанавливает текст entringStringLabel и lineEdit в зависимости от chooseSettingComboBox
+        """
         start_text = 'Введите данные для поиска '
         self.ui.entringStringlineEdit.setText('')
         self.ui.entringStringLabel.setText(start_text+self.ui.chooseSettingComboBox.currentText())
@@ -53,7 +78,9 @@ class Form_backend(QtWidgets.QMainWindow):
             self.kind_of_search = 3
 
     def changeDir(self):
-        """получение пути к выбранному в treeview каталогу"""
+        """
+        получение пути к выбранному в treeview каталогу
+        """
         model = self.ui.treeView.model()
         dir = QtWidgets.QFileSystemModel(model).filePath(self.ui.treeView.selectedIndexes()[0])
         if os.path.isfile(dir):
@@ -61,7 +88,9 @@ class Form_backend(QtWidgets.QMainWindow):
         self.ui.selectedDir_lineEdit.setText(str(dir))
 
     def setValuesForFindeFileThread(self) -> bool:
-        """установка начальных значений для потока в зависимости от выбранного вида поиска"""
+        """
+        установка начальных значений для потока в зависимости от выбранного вида поиска
+        """
         # если не выбрана директория или не заданы условия поска
         if not self.ui.entringStringlineEdit.text().split() or not self.ui.selectedDir_lineEdit.text():
             return False
@@ -105,16 +134,13 @@ class Form_backend(QtWidgets.QMainWindow):
         return True
 
 
-    def initThreads(self):
-        """инициализация потока"""
-        self.findfileThread = TFindFileThread()
-        self.findfileThread.infoSignal.connect(self.addItemToResultTable)
-        self.findfileThread.statusSignal.connect(self.showProcessInStatusBar)
-        self.findfileThread.finished.connect(self.stopSearchButtonClick)
+
 
     def startSearchButtonClick(self):
-        """изменяет состояние визуальных элементов и запускает поток"""
-        if self.ui.tableView.model().rowCount()>0:
+        """
+        изменяет состояние визуальных элементов и запускает поток
+        """
+        if self.ui.tableView.model().rowCount() > 0:
             self.ui.tableView.model().removeRows(0, self.ui.tableView.model().rowCount())
         if self.setValuesForFindeFileThread():
             self.ui.startSearchpushButton.setEnabled(False)
@@ -127,7 +153,9 @@ class Form_backend(QtWidgets.QMainWindow):
             self.ui.statusbar.showMessage('Поиск начат')
 
     def stopSearchButtonClick(self):
-        """изменяет состояние визуальных элементов и останавливает поток"""
+        """
+        изменяет состояние визуальных элементов и останавливает поток
+        """
         self.ui.startSearchpushButton.setEnabled(True)
         self.ui.stopSearchpushButton.setEnabled(False)
         self.ui.chooseSettingComboBox.setEnabled(True)
@@ -138,22 +166,141 @@ class Form_backend(QtWidgets.QMainWindow):
 
 
     def createQStandardItemModel(self) -> QtGui.QStandardItemModel:
+        """
+        создание и заполнение заголовка результирующей таблицы
+        """
         sim = QtGui.QStandardItemModel()
         sim.setHorizontalHeaderLabels(["Имя файла", "Размер", "Время создания", "Время модификации", "Время последнего доступа"])
 
         return sim
 
     def addItemToResultTable(self, info: list):
-        """Сигнал,добавляет в таблицу информацию о найденных файлах"""
+        """
+        Сигнал,добавляет в таблицу информацию о найденных файлах
+        """
         list_item = []
         for s in info:
-            list_item.append(QtGui.QStandardItem(f'{s}'))
+            item = QtGui.QStandardItem(f'{s}')
+
+            list_item.append(item)
         self.ui.tableView.model().appendRow(list_item)
 
     def showProcessInStatusBar(self, statStr: str):
+        """
+        Отображение текущего положения дел в потоке
+        :param statStr: сообщение из потока
+        """
         self.ui.statusbar.showMessage(statStr)
 
+    def copySeelectedFiles(self):
+        """
+        копирование обнрауженных файлов с вызовом диалога для определения пути сохранения файлов
+        копируются только выделенные строки в таблице
+        """
+        dir_ = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку для сохранения")
+        if dir_ != '' :
+            listQModelIndex = self.ui.tableView.selectionModel().selectedIndexes()
+            for i in listQModelIndex:
+                # проверяем существование исходного файла
+                self.src = self.ui.tableView.model().index(i.row(), 0).data()
+                if os.path.isfile(self.src):
+                    self.dest = dir_+'/'+os.path.basename(self.src)
+                    # если файла не существует
+                    if not os.path.isfile(self.dest):
+                        shutil.copy(self.src, self.dest)
+                    else:
+                    # если файл с таким именем уже существует:
+                        msg = QtWidgets.QMessageBox()
+                        msg.setText(f'Файл {self.dest} уже существует?')
+                        msg.setInformativeText('Что же делать?')
+                        msg.setIcon(QtWidgets.QMessageBox.Information)
+                        rwButton = QtWidgets.QPushButton(msg)
+                        rwButton.setText('Перезаписать')
+                        rwButton.clicked.connect(rewrite)
+                        msg.addButton(rwButton, QtWidgets.QMessageBox.ActionRole)
+                        rnButton = QtWidgets.QPushButton(msg)
+                        rnButton.setText('Переименовать')
+                        rnButton.clicked.connect(rename)
+                        msg.addButton(rnButton, QtWidgets.QMessageBox.ActionRole)
+                        sButton = QtWidgets.QPushButton(msg)
+                        sButton.setText('Пропустить')
+                        #sButton.clicked.connect(pass)
+                        msg.addButton(sButton, QtWidgets.QMessageBox.ActionRole)
+                        msg.exec()
+                    #
+                    #
+                    #     while not os.path.isfile(dest):
+                    #         dest.replace('{}')
+            msbx = QtWidgets.QMessageBox()
+            msbx.setText('Завершено')
+            msbx.setStandardButtons(QtWidgets.QMessageBox.Ok)
+            msbx.setIcon(QtWidgets.QMessageBox.Information)
+            msbx.exec()
+
+    def rewrite(self) -> bool:
+        """
+        Копирование файла с перезаписью
+        :return: результат копировнаия (успех / неудача)
+        """
+        try:
+            shutil.copy(self.src, self.dest)
+            return True
+        except IOError as err:
+            self.errorMsg.showMessage(str(err))
+            return False
+
+    def rename(self) -> bool:
+        """
+        копирование с переименованием файла в приемнике
+        :return: результат копировнаия (успех / неудача)
+        """
+        j = 1
+        self.dest = self.dest + f'({j})'
+        while os.path.isfile(self.dest):
+            self.dest.replace(f'({j})', f'({j + 1})')
+            j += 1
+        return self.rewrite()
+
+    def show_preview_window(self, fname: str):
+        """
+        Открывает новое окно и выводит содержимое файла
+        :param fname: имя файла для отображения
+        """
+        if os.stat(fname).st_size < 10737410:  # 10 Mb
+            self.preview_w = PrevieWindow(fname)
+            self.preview_w.read_file()
+            self.preview_w.show()
+        else:
+            msg = QtWidgets.QMessageBox()
+            msg.setText("Размер файла более 10 Мб")
+            msg.setInformativeText("Я отказываюсь его открывать в предпросмотре")
+            msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+            msg.exec()
+
+    def contextMenuEvent(self, event: QtGui.QContextMenuEvent) -> None:
+        """
+        Добавление контекстного меню
+        """
+        contextMenu = QtWidgets.QMenu(self.ui.tableView)
+        copyAct = contextMenu.addAction("Копировать в ...")
+        openAct = contextMenu.addAction("Посмотреть содержимое")
+        action = contextMenu.exec(self.mapToGlobal(event.pos()))
+        if action == copyAct:
+            self.copySeelectedFiles()
+        elif action == openAct:
+            listQModelIndex = self.ui.tableView.selectionModel().selectedIndexes()
+            fname = self.ui.tableView.model().index(listQModelIndex[0].row(), 0).data()
+            self.show_preview_window(fname)
+
+    def table_view_dbl_click(self):
+        listQModelIndex = self.ui.tableView.selectionModel().selectedIndexes()
+        fname = self.ui.tableView.model().index(listQModelIndex[0].row(), 0).data()
+        self.show_preview_window(fname)
+
     def event(self, event: QtCore.QEvent) -> bool:
+        """
+        Обработка событий
+        """
         if event.type() == QtCore.QEvent.Close:
             msg = QtWidgets.QMessageBox()
             msg.setText("Вы уверены, что хотите выйти?")
@@ -161,15 +308,20 @@ class Form_backend(QtWidgets.QMainWindow):
             msg.setStandardButtons(QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
             reply = msg.exec()
             if reply == QtWidgets.QMessageBox.Yes:
-                self.findfileThread.Flag = False
-                time.sleep(1) # задержка чтобы процесс успел остановиться
-                self.findfileThread.terminate()
+                if self.findfileThread.Flag:
+                    self.findfileThread.Flag = False
+                    time.sleep(1) # задержка чтобы процесс успел остановиться
+                    self.findfileThread.terminate()
                 event.accept()
-            else:
+            elif reply == QtWidgets.QMessageBox.No:
                 event.ignore()
         return QtWidgets.QWidget.event(self, event)
 
+
 class TFindFileThread(QtCore.QThread):
+    """
+    поток поиска файлов с рекурсией
+    """
     infoSignal = QtCore.Signal(list)
     statusSignal = QtCore.Signal(str)
 
@@ -194,37 +346,7 @@ class TFindFileThread(QtCore.QThread):
             self.ext = ['*.*']
         self.run_fast_scandir(self.startDir, self.ext)
 
-    @staticmethod
-    def detect_code(fname: str, size: int) -> str:
-        """
-        получение кодировки файла (не 100% решение)
-        :param fname: имя файла (полный путь)
-        :return: название кодировки
-        """
-        encoding = [
-            'utf-8',
-            'cp500',
-            'utf-16',
-            'GBK',
-            'windows-1251',
-            'ASCII',
-            'US-ASCII',
-            'Big5'
-        ]
 
-        if size > 1024:
-            size = 1024
-        for enc in encoding:
-            try:
-                with open(fname, 'r', encoding=enc) as fr:
-                    fr.read(size)
-            except (UnicodeDecodeError, LookupError):
-                pass
-            except PermissionError:
-                print('Отказано в доступе')
-            else:
-                return enc
-            return None
 
     def run_fast_scandir(self, dir, ext) -> list:
         """
@@ -236,27 +358,33 @@ class TFindFileThread(QtCore.QThread):
         subfolders, files = [], []
 
         def add_item():
-            """добавление полученных данных в таблицу"""
+            """
+            передача данных в сигнал
+            """
             ctime = datetime.datetime.fromtimestamp(f.stat().st_ctime).strftime("%d-%m-%Y %H:%M:%S")
             mtime = datetime.datetime.fromtimestamp(f.stat().st_mtime).strftime("%d-%m-%Y %H:%M:%S")
             atime = datetime.datetime.fromtimestamp(f.stat().st_atime).strftime("%d-%m-%Y %H:%M:%S")
             self.infoSignal.emit([f.path, f"{f.stat().st_size} байт", ctime, mtime, atime])
 
         def find_signature() -> bool:
-            """производит поиск сигнатуры в файле"""
+            """
+            поиск сигнатуры в файле
+            """
             with open(f.path, "rb") as sf:
                 if self.signature == sf.read(len(self.signature)):
                     return True
             return False
 
         def find_string_in_file() -> bool:
-            """поиск ключевого слова в файле"""
+            """
+            поиск ключевого слова в файле
+            """
             # определяем кодировку файла
             self.statusSignal.emit(f'Поиск в файле {f.path}')
             size = f.stat().st_size
             if size == 0:
                 return False
-            code_file = self.detect_code(f.path, size)
+            code_file = PrevieWindow.detect_code(f.path, size)
 
             if code_file is None:
                 return False
@@ -287,8 +415,6 @@ class TFindFileThread(QtCore.QThread):
                 print(f"{f.path}: недостаточно прав")
                 return False
 
-
-
         try:
             for f in os.scandir(dir):
                 self.statusSignal.emit(f'Поиск в директории {dir}')
@@ -305,7 +431,6 @@ class TFindFileThread(QtCore.QThread):
                     if self.flag_keyword and find_string_in_file():
                         add_item()
 
-                        #files.append(f.path)
                 if not (self.Flag):
                     os.scandir().close()
                     break
